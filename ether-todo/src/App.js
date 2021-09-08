abdc76793e35a328609c2977ef375d42ebf1eefb
kapilchandrawal <77915124+kapilchandrawal@users.noreply.github.com>
2021-09-08 13:36:40 +0530

    Convert class into function component

--- ether-todo/src/App.js
+++ ether-todo/src/App.js
@@ -1,52 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import Web3 from 'web3';
 import './App.css';
 
-class App extends Component {
-  componentWillMount() {
-    this.loadBlockchainData()
-  }
+export default function App() {
 
-  async loadBlockchainData() {
-    const web3 = new Web3(Web3.givenProvider)
-    // || "http://localhost:8545"
+  const [network, setNetwork] = useState('');
+  const [accounts, setAccount] = useState('');
+  const [balance, setBalance] = useState('');
 
-    const network = await web3.eth.net.getNetworkType()
-    console.log("network:", network)
+  useEffect(() => {
 
-    const accounts = await web3.eth.getAccounts()
-    console.log("Account:", accounts)
+    async function loadBlockchainData() {
+      const web3 = new Web3(Web3.givenProvider)
+      // || "http://localhost:8545"
 
-    const balance = await web3.eth.getBalance(accounts[0])
-    console.log("balance:", balance)
+      const network = await web3.eth.net.getNetworkType()
+      setNetwork(network)
+      console.log("network:", network)
 
-    this.setState({
-      network: network,
-      account: accounts,
-      balance: balance
-    })
-  }
+      const accounts = await web3.eth.getAccounts()
+      setAccount(accounts)
+      console.log("Account:", accounts)
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      account: '',
-      balance: ''
+      const balance = await web3.eth.getBalance(accounts[0])
+      setBalance(balance)
+      console.log("balance:", balance)
     }
-  }
-  render() {
-    return (
-      <div className="container">
-        <h1>Welcome</h1>
-        <p>Your network : {this.state.network}</p>
-        <p>Your account : {this.state.account}</p>
-        <p>Your Balance : {this.state.balance}</p>
-        {/* <button onClick={() => {
+    loadBlockchainData()
+  },)
+
+  return (
+
+    <div className="container">
+      <h1>Welcome</h1>
+      <p>Your network : {network}</p>
+      <p>Your account : {accounts}</p>
+      <p>Your Balance : {balance}</p>
+      {/* <button onClick={() => {
           ethereum.request({ method: 'eth_requestAccounts' });
         }}>Enable Ethereum</button> */}
-      </div>
-    );
-  }
-}
+    </div>
 
-export default App;
+  )
+}
