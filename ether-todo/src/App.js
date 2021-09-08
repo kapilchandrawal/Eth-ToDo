c6cf6f32c991b7e57f739abe964620678bd02ad5
kapilchandrawal <77915124+kapilchandrawal@users.noreply.github.com>
2021-09-08 12:57:08 +0530

    create class component for showing address, network and balance

--- ether-todo/src/App.js
+++ ether-todo/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Web3 from 'web3';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentWillMount() {
+    this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider)
+    // || "http://localhost:8545"
+
+    const network = await web3.eth.net.getNetworkType()
+    console.log("network:", network)
+
+    const accounts = await web3.eth.getAccounts()
+    console.log("Account:", accounts)
+
+    const balance = await web3.eth.getBalance(accounts[0])
+    console.log("balance:", balance)
+
+    this.setState({
+      network: network,
+      account: accounts,
+      balance: balance
+    })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      balance: ''
+    }
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1>Welcome</h1>
+        <p>Your network : {this.state.network}</p>
+        <p>Your account : {this.state.account}</p>
+        <p>Your Balance : {this.state.balance}</p>
+        {/* <button onClick={() => {
+          ethereum.request({ method: 'eth_requestAccounts' });
+        }}>Enable Ethereum</button> */}
+      </div>
+    );
+  }
 }
 
 export default App;
